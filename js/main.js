17c02b728200f162cf5cec9c779f4dc6be91b979
mehrabsha <mehrab572@gmail.com>
2020-08-12 18:29:16 +0430

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 particlesJS.load('section1', 'assets/config.json', function() {
     console.log('callback - particles.js config loaded');
 });
-
 function smoothScroll(){
     window.scrollBy({ 
         top: document.getElementById("section2").offsetTop, // could be negative value
@@ -9,7 +8,6 @@ function smoothScroll(){
         behavior: 'smooth' 
       });
 }
-
 function openMenu(){
         var ul = document.querySelector("nav ul");
         var ulli = document.querySelectorAll("nav ul li");
@@ -30,8 +28,18 @@ function openMenu(){
     }
 
 }
-
-
+function goTo(event){
+    console.log(document.getElementById(event.getAttribute('target')).offsetTop);
+    window.scrollTo({ 
+        top: document.getElementById(event.getAttribute('target')).offsetTop, // could be negative value
+        left: 0, 
+        behavior: 'smooth' 
+      });
+     
+      if (window.screen.width < 768){
+        openMenu();
+      }
+}
 document.addEventListener("DOMContentLoaded", function(event) {
     // Your code to run since DOM is loaded and ready
 
@@ -45,47 +53,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
         strings: ['WEB DEVELOPER', 'UI DESIGNER' , 'ANDROID DEVELOPER'],
         autoStart: true,
         loop : true
-      });
+    });
 });
-
-/*
-
-var i =000 ;
-var sec1 = document.querySelector('#section1');
-var sec2 = document.querySelector('#section2');
-var sec3 = document.querySelector('#section3');
-var sec4 = document.querySelector('#section4');
-var sec5 = document.querySelector('#section5');
-var sec6 = document.querySelector('#section6');
-
-var setions = [sec1,sec2,sec3,sec4,sec5];
-const sec2top = sec2.offsetTop;
-const sec3top = sec3.offsetTop;
-
-
-window.addEventListener("scroll", function (event) {
-        var scroll = this.scrollY;
-        console.log(window.scrollY + " || " + sec3top)
-
-        
-    if (sec2.offsetTop < window.scrollY + 50 && sec2.offsetTop > window.scrollY - 50 && sec2.style.position != 'fixed'){
-        sec2.style.position = 'fixed';
-        sec2.style.marginTop = '0';
-        sec3.style.marginTop = '200vh';
-    }else if ( window.scrollY < sec2top && sec2.style.position == 'fixed'){
-        sec2.style.position = 'relative';
-        sec2.style.marginTop = '100vh';
-        sec3.style.marginTop = '0';
-    }
-
-    if (sec3top < window.scrollY + 50 && sec3top > window.scrollY - 50 && sec3.style.position != 'fixed'){
-        sec3.style.position = 'fixed';
-        sec3.style.marginTop = '0';
-        sec4.style.marginTop = '100vh';
-    }else if ( window.scrollY < sec3top && sec3.style.position == 'fixed'){
-        sec3.style.position = 'relative';
-        sec3.style.marginTop = '0';
-        sec4.style.marginTop = '0';
-    }
-
-});*/
